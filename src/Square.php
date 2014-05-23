a1afe5c44beaf22fd9a1023b64a1cc24783a98f9
Jason Judge <jason.judge@consil.co.uk>
2014-05-24 00:51:01 +0100

    All the tools in Square to convert between any digit, numeric or letter combinations.

--- src/Square.php
+++ src/Square.php
@@ -53,10 +53,10 @@ class Square
 
     /**
      * The letters used to name squares, in a 5x5 grid.
-     * 'A' is the top left (North-West).
+     * 'V' is the bottom left (South-West).
      */
 
-    public static $letters = 'ABCDEFGHJKLMNOPQRSTUVWXYZ';
+    public static $letters = 'VWXYZQRSTULMNOPFGHJKABCDE';
 
     /**
      * Convert a letter to its Eastern zero-based postion in a 25x25 grid
@@ -74,7 +74,7 @@ class Square
 
     public static function letterNorthPosition($letter)
     {
-        return 4 - floor(strpos(static::$letters, strtoupper($letter)) / 5);
+        return floor(strpos(static::$letters, strtoupper($letter)) / 5);
     }
 
     /**
@@ -203,6 +203,112 @@ class Square
         $north += static::digitsToDistance($digits, strlen($letters));
 
         return $north;
+    }
+
+    /**
+     * Convert an ABS East/North value pair into letters.
+     * The number of letters is 0, 1 or 2.
+     */
+
+    public static function absToLetters($abs_east, $abs_north, $number_of_letters)
+    {
+        // TODO: if no letters are needed, then we are dealing with seven-digit numbers
+        // based on the square SV origin. Make sure the ABS east and north values are
+        // positive wrt square SV. This system is designed to avoid handling negative numbers
+        // in all situations.
+
+        $letters = array();
+
+        if ($number_of_letters >= 1) {
+            // Get the first letter (we have at least one).
+            // Find the position on the 5x5 500km grid.
+            $east_500_position = floor($abs_east / 500000);
+            $north_500_position = floor($abs_north / 500000);
+
+            $letters[] = static::$letters[($north_500_position * 5) + $east_500_position];
+        }
+
+        if ($number_of_letters >= 2) {
+            // Get the second letter.
+            // Find the position on the 5x5 100km grid, within the 500km grid.
+            $east_100_position = floor(($abs_east - 500000 * $east_500_position) / 100000);
+            $north_100_position = floor(($abs_north - 500000 * $north_500_position) / 100000);
+
+            $letters[] = static::$letters[($north_100_position * 5) + $east_100_position];
+        }
+
+        return implode('', $letters);
+    }
+
+    /**
+     * Convert an ABS East value into digits.
+     * The number of letters we are using with the digits is 0, 1 or 2.
+     * The number of digits is between 0 and 7, but the number of digits and
+     * the number of letters combined must not be more than 7.
+     * If the number of letters is zero, then the assumed letter origin will
+     * be 500km square 'S'.
+     */
+
+    public static function absEastToDigits($abs_east, $number_of_letters, $number_of_digits)
+    {
+        switch ($number_of_letters) {
+            case 0:
+                // No letters, so an actual number of metres East square S.
+                $offset = $abs_east - static::$gb_origin_east;
+                break;
+
+            case 1:
+                // One letter, so an offset within the 500km box.
+                $offset = $abs_east % 500000;
+                break;
+
+            case 2:
+                // Two letters, so an offset within a 100km box.
+                $offset = $abs_east % 100000;
+                break;
+        }
+
+        // Knock some digits off if it comes to greater than 7, when counting the letters too.
+        if ($number_of_letters + $number_of_digits > 7) {
+            $number_of_digits = 7 - $number_of_letters;
+        }
+
+        // Left-pad the number to 5, 6, or 7 digits, depending on the number of letters.
+        $digits = str_pad((string)$offset, 7 - $number_of_letters, '0', STR_PAD_LEFT);
+
+        // Now take only the required significant digits.
+        return substr($digits, 0, $number_of_digits);
+    }
+
+    public static function absNorthToDigits($abs_north, $number_of_letters, $number_of_digits)
+    {
+        switch ($number_of_letters) {
+            case 0:
+                // No letters, so an actual number of metres East square S.
+                $offset = $abs_north - static::$gb_origin_north;
+                break;
+
+            case 1:
+                // One letter, so an offset within the 500km box.
+                $offset = $abs_north % 500000;
+                break;
+
+            case 2:
+                // Two letters, so an offset within a 100km box.
+                $offset = $abs_north % 100000;
+                break;
+        }
+
+        // Knock some digits off if it comes to greater than 7, when counting the letters too.
+        if ($number_of_letters + $number_of_digits > 7) {
+            $number_of_digits = 7 - $number_of_letters;
+        }
+
+        // Left-pad the number to 5, 6, or 7 digits, depending on the number of letters.
+        $digits = str_pad((string)$offset, 7 - $number_of_letters, '0', STR_PAD_LEFT);
+
+        // Now take only the required significant digits.
+        return substr($digits, 0, $number_of_digits);
     }
 }
 
